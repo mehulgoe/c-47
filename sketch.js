dece64916b1d6aa16dd2e17a0701de73c2e4cf91
mehulgoe <78550312+mehulgoe@users.noreply.github.com>
2022-01-11 14:48:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,214 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+var heart1,heart2,heart3;
+var heart1img,heart2img,heart3img
+var zombieGroup
+var score = 0;
+var lives = 3;
+var bullets = 70;
+var heart1,herat2,heart3
+var gamestate = "fight"
+var lose,winning,explosionSound;
+function preload(){
+  heart1img = loadImage("assets/heart_1.png")
+  heart2img = loadImage("assets/heart_2.png")
+  heart3img = loadImage("assets/heart_3.png")
+  shooterImg = loadImage("assets/shooter_2.png")
+  shooter_shooting = loadImage("assets/shooter_3.png")
+  bgImg = loadImage("assets/bg.jpeg")
+  zombieImg = loadImage("assets/zombie.png")
+lose = loadSound("assets/lose.mp3")
+winning = loadSound("assets/win.mp3")
+explosionSound = loadSound("assets/explosion.mp3")
+
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adding the background image
+  bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+bg.addImage(bgImg)
+bg.scale = 1.1
+  
+
+//creating the player sprite
+player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
+ player.addImage(shooterImg)
+   player.scale = 0.3
+   player.debug = true
+   player.setCollider("rectangle",0,0,300,300)
+
+   heart1 = createSprite(displayWidth-150,40,20,20)
+   heart1.visible = false
+    heart1.addImage("heart1",heart1img)
+    heart1.scale = 0.4
+
+    heart2 = createSprite(displayWidth-100,40,20,20)
+    heart2.visible = false
+    heart2.addImage("heart2",heart2img)
+    heart2.scale = 0.4
+
+    heart3 = createSprite(displayWidth-150,40,20,20)
+    heart3.addImage("heart3",heart3img)
+    heart3.scale = 0.4
+    zombieGroup = new Group ();
+    bulletGroup = new Group ()
+
+}
+
+function draw() {
+  background(0); 
+
+if(gamestate==="fight"){
+  if(lives===3){
+    heart3.visible = true
+    heart2.visible = false
+    heart1.visible = false 
+    
+  }
+  if(lives===2){
+    heart2.visible = true
+    heart3.visible = false
+    heart1.visible = false  
+  }
+  if(lives===1){
+    heart1.visible = true
+    heart2.visible = false
+    heart3.visible = false
+
+  }
+  if(lives===0){
+  gamestate="lost"
+  }
+  if(score==100){
+    gameState = "won"
+    winning.play();
+  }
+
+  //moving the player up and down and making the game mobile compatible using touches
+if(keyDown("UP_ARROW")||touches.length>0){
+  player.y = player.y-30
+}
+if(keyDown("DOWN_ARROW")||touches.length>0){
+ player.y = player.y+30
+}
+
+
+//release bullets and change the image of shooter to shooting position when space is pressed
+if(keyWentDown("space")){
+  bullet = createSprite(displayWidth-1150,player.y-30,20,10)
+  bullet.velocityX = 20
+  
+  bulletGroup.add(bullet)
+  player.depth = bullet.depth
+  player.depth = player.depth+2
+  player.addImage(shooter_shooting)
+  bullets = bullets-1
+  explosionSound.play();
+}
+
+//player goes back to original standing image once we stop pressing the space bar
+else if(keyWentUp("space")){
+  player.addImage(shooterImg)
+}
+if(bullets==0){
+  gameState = "bullet"
+  lose.play();
+    
+}
+
+if(zombieGroup.isTouching(bulletGroup)){
+  for(var i=0;i<zombieGroup.length;i++){     
+      
+   if(zombieGroup[i].isTouching(bulletGroup)){
+        zombieGroup[i].destroy()
+        bulletGroup.destroyEach()
+        explosionSound.play();
+ 
+        score = score+2
+        } 
+  
+  }
+}
+
+
+if(zombieGroup.isTouching(player)){
+ 
+  lose.play();
+
+
+for(var i=0;i<zombieGroup.length;i++){     
+     
+ if(zombieGroup[i].isTouching(player)){
+      zombieGroup[i].destroy()
+     
+     lives=lives-1
+      } 
+
+}
+}
+
+//calling the function to spawn zombies
+enemy();
+}
+drawSprites();
+
+//displaying the score and remaining lives and bullets
+textSize(20)
+  fill("white")
+text("Bullets = " + bullets,displayWidth-210,displayHeight/2-250)
+text("Score = " + score,displayWidth-200,displayHeight/2-220)
+text("Lives = " + lives,displayWidth-200,displayHeight/2-280)
+if(gamestate == "lost"){
+  
+  textSize(100)
+  fill("red")
+  text("You Lost ",400,400)
+  zombieGroup.destroyEach();
+  player.destroy();
+
+}
+
+else if(gamestate == "won"){
+ 
+  textSize(100)
+  fill("yellow")
+  text("You Won ",400,400)
+  zombieGroup.destroyEach();
+  player.destroy();
+
+}
+
+else if(gamestate == "bullet"){
+ 
+  textSize(50)
+  fill("yellow")
+  text("You ran out of bullets!!!",470,410)
+  zombieGroup.destroyEach();
+  player.destroy();
+  bulletGroup.destroyEach();
+
+}
+
+}
+
+function enemy(){
+  if(frameCount%50===0){
+
+    //giving random x and y positions for zombie to appear
+    zombie = createSprite(random(500,1100),random(100,500),40,40)
+
+    zombie.addImage(zombieImg)
+    zombie.scale = 0.15
+    zombie.velocityX = -3
+    zombie.debug= true
+    zombie.setCollider("rectangle",0,0,400,400)
+   
+    zombie.lifetime = 400
+   zombieGroup.add(zombie)
+  }
+
+}
